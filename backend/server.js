d59186014b82278ad1971a436261bf218d7c168b
AbdulWahabMunir <123945127+AbdulWahabMunir@users.noreply.github.com>
2026-01-04 21:14:01 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -73,9 +73,8 @@ app.use((err, req, res, next) => {
 })
 
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server is running on port ${PORT}`)
-  console.log(`📝 API available at http://localhost:${PORT}`)
 })
 
 
